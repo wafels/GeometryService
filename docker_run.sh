e6acbedb64cd33b890886bfe716714399d172c18
Bogdan Nicula <bogdan@nicula.net>
2017-05-30 12:34:09 +0200

    Fix ports
    Former-commit-id: 5f4c4b434694ff258a7e67d6131960a3db18ed49

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -it -p 80:80 --rm --name=geometry-service geometry-service /geometry/python/bin/geometry_service
+docker run -it -p 7789:7789 --rm --name=geometry-service geometry-service /geometry/python/bin/geometry_service
