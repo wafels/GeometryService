8b9f192621616ca00e667ac78535b4d34a3bf5eb
Bogdan Nicula <bogdan@nicula.net>
2018-11-15 12:56:21 +0100

    Update kernels
    Former-commit-id: 3c7314d231a1dd3047d89d56de060b1cae161d3c

---
+++ data/kernels/psp/fk/psp.tf
@@ -0,0 +1,11 @@
+KPL/FK
+
+\begindata
+
+    NAIF_BODY_NAME += ( 'PSP' )
+    NAIF_BODY_CODE += ( -96 )
+
+    NAIF_BODY_NAME += ( 'PARKER SOLAR PROBE' )
+    NAIF_BODY_CODE += ( -96 )
+
+\begintext
